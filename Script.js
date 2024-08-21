3a7d33dd8c5cd1b54a0d5213293f4bb4bd85c660
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-08-20 22:41:27 -0500

    me emocione y lo termine haciendo todo

---
+++ Script.js
@@ -0,0 +1,24 @@
+let i = 0;
+const images = ["image_1.jpg", "image_2.jpeg", "image_3.jpeg"];
+
+function next_image() {
+    if (i < images.length-1) {
+        i++;
+        document.getElementById("image").src = images[i];
+        document.getElementById("previous").style.color = "white";
+    }
+    else {
+        document.getElementById("next").style.color = "#0011ff";
+    }
+}
+
+function previous_image() {
+    if (i > 0) {
+        i--;
+        document.getElementById("image").src = images[i];
+        document.getElementById("next").style.color = "white";
+    }
+    else {
+        document.getElementById("previous").style.color = "black";
+    }
+}
